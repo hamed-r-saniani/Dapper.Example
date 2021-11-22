dc9fd209eccfe6e761953502567a7c95ae6e3ed4
Hamed Rezaie Saniani <Hamed.r.Saniani@outlook.com>
2021-11-22 23:47:16 +0330

    Add project files.

---
+++ Dapper.Example/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dapper.Example.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ Dapper.Example/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dapper.Example.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
