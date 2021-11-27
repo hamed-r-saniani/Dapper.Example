1e99b9fc986017892ebdcc99f0e98d64d08d8bfb
Hamed Rezaie Saniani <Hamed.r.Saniani@outlook.com>
2021-11-27 23:09:29 +0330

    Implemented EmployeesController

---
+++ Dapper.Example/Controllers/EmployeesController.cs
@@ -0,0 +1,148 @@
+﻿using Dapper.Example.Models;
+using Dapper.Example.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Dapper.Example.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ICompanyRepository _companyRepository;
+        
+        public EmployeesController(IEmployeeRepository context, ICompanyRepository companyRepository)
+        {
+            _employeeRepository = context;
+            _companyRepository = companyRepository;
+        }
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(_employeeRepository.GetAll());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Employee = _employeeRepository.Find(id.Value);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(Employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public async Task<IActionResult> CreatePost()
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeRepository.Add(Employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Employee = _employeeRepository.Find(id.Value);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            return View(Employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id != Employee.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _employeeRepository.Update(Employee);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(Employee.EmployeeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Employee = _employeeRepository.Find(id.Value);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(Employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            _employeeRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _employeeRepository.IsExists(id);
+        }
+    }
+}
