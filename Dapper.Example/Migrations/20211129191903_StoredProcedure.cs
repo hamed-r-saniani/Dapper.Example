d7fc5f5cf9628bcad7756e4d795da1b12a40b806
Hamed Rezaie Saniani <Hamed.r.Saniani@outlook.com>
2021-11-29 23:11:27 +0330

    Create Company StoreProcedures With Migration

---
+++ Dapper.Example/Migrations/20211129191903_StoredProcedure.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dapper.Example.Migrations
+{
+    public partial class StoredProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                 Create PROC usp_GetCompany
+                 @CompanyId int
+                 AS
+                 BEGIN
+                 Select * From Companies
+                 Where CompanyId = @CompanyId
+                 END
+                 GO
+            ");
+            migrationBuilder.Sql(@"
+                 Create PROC usp_GetAllCompany
+                 AS
+                 BEGIN
+                 Select * From Companies
+                 END
+                 GO
+            ");
+            migrationBuilder.Sql(@"
+                 Create PROC usp_AddCompany
+                 @CompanyId int OUTPUT,
+                 @Name varchar(MAX),
+                 @Address varchar(MAX),
+                 @City varchar(MAX),
+                 @State varchar(MAX),
+                 @PostalCode varchar(MAX)
+                 AS
+                 BEGIN
+                 Insert Into Companies(Name,Address,City,State,PostalCode)
+                 VALUES(@Name,@Address,@City,@State,@PostalCode);
+                 Select @CompanyId = SCOPE_IDENTITY()
+                 END
+                 GO
+            ");
+            migrationBuilder.Sql(@"
+                 Create PROC usp_UpdateCompany
+                 @CompanyId int,
+                 @Name varchar(MAX),
+                 @Address varchar(MAX),
+                 @City varchar(MAX),
+                 @State varchar(MAX),
+                 @PostalCode varchar(MAX)
+                 AS
+                 BEGIN
+                 UPDATE Companies SET Name = @Name,Address = @Address,City = @City,State = @State,PostalCode = @PostalCode
+                 WHERE CompanyId = @CompanyId;
+                 END
+                 GO
+            ");
+            migrationBuilder.Sql(@"
+                 Create PROC usp_DeleteCompany
+                 @CompanyId int
+                 AS
+                 BEGIN
+                 DELETE From Companies WHERE CompanyId = @CompanyId
+                 END
+                 GO
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
